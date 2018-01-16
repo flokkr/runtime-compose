1ca556f406ae64b0df537798662d70f3a18d2ffd
Márton Elek <elek@apache.org>
2018-01-16 12:13:07 +0100

    hdfs federation example

---
+++ hdfs-federation/namenodeformat.sh
@@ -0,0 +1,13 @@
+#/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -x
+if [ -n "$ENSURE_NAMENODE_DIR" ]; then
+   CLUSTERID_OPTS=""
+   if [ -n "$ENSURE_NAMENODE_CLUSTERID" ]; then
+      CLUSTERID_OPTS="-clusterid $ENSURE_NAMENODE_CLUSTERID"
+   fi
+   if [ ! -d "$ENSURE_NAMENODE_DIR" ]; then
+      /opt/hadoop/bin/hdfs namenode -format $CLUSTERID_OPTS
+        fi
+fi
+call-next-plugin "$@"
