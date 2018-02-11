3b48a2f600a36048e933f5126d37e357421933e7
Márton Elek <elek@apache.org>
2018-02-11 16:23:56 +0100

    finalized working demo for ozone file system + spark

--- spark-ozone/initdata.sh
+++ spark-ozone/initdata.sh
@@ -3,10 +3,17 @@
 export HADOOP_CLASSPATH=/opt/hadoop/share/hadoop/tools/lib/hadoop-ozone-3.1.0-SNAPSHOT.jar 
 
 ls > /tmp/file
-hdfs dfs -mkdir /sparklog
-hdfs dfs -chmod 777 /sparklog
-hdfs dfs -chmod 777 /
+
 hdfs oz -createVolume http://datanode:9864/test -user tester -quota 100TB -root
 hdfs oz -createBucket http://datanode:9864/test/bucket1
 hdfs oz -putKey http://datanode:9864/test/bucket1/key1 --file /tmp/file
+
+#hdfs dfs -mkdir hdfs://namenode:9000/sparklog
+#hdfs dfs -chmod 777 hdfs://namenode:9000/sparklog
+#hdfs dfs -chmod 777 hdfs://namenode:9000/
+
+hdfs dfs -mkdir /sparklog
+hdfs dfs -chmod 777 /sparklog
+hdfs dfs -chmod 777 /
+
 
--- spark-ozone/run.sh
+++ spark-ozone/run.sh
@@ -6,6 +6,14 @@ export SPARK_DIST_CLASSPATH="$(hadoop classpath):/opt/hadoop/share/hadoop/tools/
 /opt/spark/bin/spark-submit \
   --deploy-mode cluster \
   --master yarn \
+  --class org.apache.spark.examples.JavaWordCount /opt/spark/examples/jars/spark-examples_2.11-2.2.1.jar \
+  /alice.txt
+
+
+
+/opt/spark/bin/spark-submit \
+  --deploy-mode cluster \
+  --master yarn \
   --files file:///opt/spark/conf/log4j.properties  \
   --driver-java-options "-Dlog4j.configuration=./log4j.properties"  \
   --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=./log4j.properties"  \
@@ -15,6 +23,7 @@ export SPARK_DIST_CLASSPATH="$(hadoop classpath):/opt/hadoop/share/hadoop/tools/
 
 
 
+#
 #  --conf 'spark.hadoop.fs.defaultFS=o3://datanode:9874/test/bucket1/' \
 #  --conf 'spark.executor.extraJavaOptions=-Dfs.o3.impl=org.apache.hadoop.fs.ozone.OzoneFileSystem' \
 #  --conf 'spark.driver.extraJavaOptions=-Dfs.o3.impl=org.apache.hadoop.fs.ozone.OzoneFileSystem' \
