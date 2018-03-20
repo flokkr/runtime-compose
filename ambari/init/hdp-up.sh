317201ab1ac58c447e9020d40951f44d4ee72ec6
Márton Elek <elek@apache.org>
2018-03-20 09:24:32 +0100

    cleanup ambari startup scripts

--- ambari/init/hdp-up.sh
+++ ambari/init/hdp-up.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
-
-curl -v -k -u admin:admin -H "X-Requested-By:ambari" -X POST --data @hdfs-blueprint.json  http://localhost:8080/api/v1/blueprints/hdfs
-curl -v -k -u admin:admin -H "X-Requested-By:ambari" -X POST http://localhost:8080/api/v1/version_definitions -d '{
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+curl -v -k -u admin:admin -H "X-Requested-By:ambari" -X POST --data @$DIR/hdfs-blueprint.json  http://ambari-server:8080/api/v1/blueprints/hdfs
+curl -v -k -u admin:admin -H "X-Requested-By:ambari" -X POST http://ambari-server:8080/api/v1/version_definitions -d '{
   "VersionDefinition": {
-   "version_url": "http://public-repo-1.hortonworks.com/HDP/centos6/2.x/updates/2.6.3.0/HDP-2.6.3.0-235.xml"
+   "version_url": "http://public-repo-1.hortonworks.com/HDP/centos7/2.x/updates/2.6.3.0/HDP-2.6.3.0-235.xml"
     }
   }'
+
+curl -v -k -u admin:admin -H "X-Requested-By:ambari" -X POST http://ambari-server:8080/api/v1/clusters/c1 -d '{
+  "blueprint": "hdfs",
+  "default_password": "Welcome1",
+  "repository_version_id": 1,
+  "configurations": [
+    {
+    }
+  ],
+  "host_groups": [
+    {
+      "name": "host_group_1",
+      "host_count": 1
+    }
+  ]
+}'
+
